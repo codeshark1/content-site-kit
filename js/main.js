d59620199784fb96c7774a963dab596cd1d455e1
codeshark1 <codeshark@yandex.ru>
2018-04-17 12:05:53 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -15,31 +15,6 @@ jQuery(document).ready(function($){
 
     navigation_show('#nav-toggle','.nav-menu-mob-wrapper');
 
-    function up() {
-        $('#up').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-                || location.hostname == this.hostname) {
-
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    }
-
-    function but_up() {
-        if ( $(window).scrollTop() > 100 ) {
-            $("#up").css('opacity',1);
-        } else {
-            $("#up").css('opacity',0);
-        }
-    }
-
     function menu_nested(menu_id) {
         $(menu_id).find('ul').hide();
         $(menu_id).find('.menu-item--has-children a').click(function(e){
@@ -57,11 +32,162 @@ jQuery(document).ready(function($){
     menu_nested('.menu-categs');
 
 
-    //up();
-    //but_up();
-    $(window).scroll(function() {
-        //but_up();
+
+    /* LISTING VIEW---*/
+    function unsquare() {
+        $('#card-list')
+            .removeClass('card-list-square')
+            .find('.card-list-item-square')
+            .removeClass('card-list-item-square')
+            .addClass('card-list-item');
+        $('#view-type-list').addClass('active').siblings().removeClass('active');
+    }
+
+    function view_type() {
+        $('#view-type-list').click(function(e){
+            e.preventDefault();
+            unsquare();
+        });
+
+        $('#view-type-square').click(function(e){
+            e.preventDefault();
+            $(this).addClass('active').siblings().removeClass('active');
+            $('#card-list')
+                .addClass('card-list-square')
+                .find('.card-list-item')
+                .removeClass('card-list-item')
+                .addClass('card-list-item-square');
+        });        
+    }
+    view_type();
+
+    if( $(window).width() < 992) {
+        unsquare();
+    }
+
+    $(window).resize(function(){
+        if( $(window).width() < 992) {
+            unsquare();
+        }        
+    });
+    /* --LISTING VIEW*/
+
+
+    /*FILTER--*/
+    function filter_toggle() {
+        $('#filter-extra').hide();
+        $('#filter-extra-btn').click(function(){
+            if ( $('#filter-extra').is(':visible')){
+                $('#filter-extra').slideUp();
+                $(this)
+                    .removeClass('active')
+                    .find('span')
+                    .text('Показать Весь Фильтр')
+                ;
+            } else {
+                $('#filter-extra').slideDown();
+                $(this).addClass('active')
+                    .find('span')
+                    .text('Скрыть Весь Фильтр')
+                ;
+            }
+        });
+    }
+    
+    filter_toggle();
+    /*FILTER--*/
+
+
+    /*CAROUSEL*/
+
+    $('#carousel').slick({
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      dots: false,
+      prevArrow: "<button type='button' type='button' class='slick-prev'><i class='fas fa-chevron-left'></i></button>",
+      nextArrow: "<button type='button' type='button' class='slick-next'><i class='fas fa-chevron-right'></i></button>",
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 
 
+
+
+
+
+
+
+
+    /*RANGE SLIDER*/
+    var slider_1 = document.getElementById('slider-range-1');
+    var slider_2 = document.getElementById('slider-range-2');
+    var valuesDivs_1 = [
+        document.getElementById('price-min'),
+        document.getElementById('price-max')
+    ];
+    var valuesDivs_2 = [
+        document.getElementById('invest-min'),
+        document.getElementById('invest-max')
+    ];
+
+    noUiSlider.create(slider_1, {
+        start: [10000, 500000],
+        connect: true,
+        step: 1000,
+        range: {
+            'min': 1000,
+            'max': 1000000
+        },
+        format: wNumb({
+            decimals: 0,
+            thousand: ','
+        })
+    });
+
+    noUiSlider.create(slider_2, {
+        start: [10000, 500000],
+        connect: true,
+        step: 1000,
+        range: {
+            'min': 1000,
+            'max': 1000000
+        },
+        format: wNumb({
+            decimals: 0,
+            thousand: ','
+        })
+    });
+
+    slider_1.noUiSlider.on('update', function( values, handle ) {
+        valuesDivs_1[handle].value = values[handle];
+    });
+
+    slider_2.noUiSlider.on('update', function( values, handle ) {
+        valuesDivs_2[handle].value = values[handle];
+    });
 });
